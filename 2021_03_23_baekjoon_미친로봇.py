ef81aecf1108011a1fb39a3edc8964b652e87ace
nagi930 <gnkim827@gmail.com>
2021-03-24 23:33:07 +0900

    2021_03_24

--- 2021_03_23_baekjoon_미친로봇.py
+++ 2021_03_23_baekjoon_미친로봇.py
@@ -1,22 +1,32 @@
-n, E, W, S, N = map(int, input().split())
+# 미친 로봇
+# https://www.acmicpc.net/problem/1405
+
+
+n, E, W, S, N = list(map(int, input().split()))
 cardinal = [E/100, W/100, S/100, N/100]
 
-def dfs(L, p):
-    global orders, answer
-    if ('S' in orders and 'N' in orders) or ('W' in orders and 'E' in orders):
-            return
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(L, p, xx, yy):
+    global answer
     if L == n:
-            answer += p
+        answer += p
     else:
         for i in range(4):
-            orders += c[i]
-            dfs(L+1, p*cardinal[i])
-            orders = orders[:-1]
+            x = xx + dx[i]
+            y = yy + dy[i]
+            if check[y][x] == 1:
+                continue
+            else:
+                check[y][x] = 1
+                dfs(L+1, p*cardinal[i], x, y)
+                check[y][x] = 0
 
 answer = 0
-orders = ''
-c = 'EWSN'
-dfs(0, 1)
+check = [[0] * 29 for _ in range(29)]
+check[14][14] = 1
+dfs(0, 1, 14, 14)
 
-print(f'{answer:.10f}')
+print(f'{answer:.15f}')
 
