323b9cbb7abb0beb1ba8b897d6d26862c102c106
nagi930 <gnkim827@gmail.com>
2021-04-17 23:21:46 +0900

    2021_04_17

---
+++ 2021_04_17_baekjoon_k번째수.py
@@ -0,0 +1,18 @@
+n = int(input())
+k = int(input())
+
+left = 0
+right = k
+
+while left <= right:
+    mid = (left + right) // 2
+    tmp = 0
+    for i in range(1, n+1):
+        tmp += min(mid//i, n)
+    if tmp >= k:
+        num = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(num)
