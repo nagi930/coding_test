5a84aa0bef314c85be0295f3b5ae6eba49520898
nagi930 <gnkim827@gmail.com>
2021-04-23 23:52:26 +0900

    2021_04_23

---
+++ 2021_04_23_baekjoon_AC.py
@@ -0,0 +1,36 @@
+# https://www.acmicpc.net/problem/5430
+
+from collections import deque
+
+n = int(input())
+for _ in range(n):
+    queries = list(input())
+    m = int(input())
+    nums = deque(eval(input()))
+    reverse = False
+    count = [0, 0]
+    for idx, query in enumerate(queries):
+
+        if query == 'D':
+            if reverse == True:
+                count[1] += 1
+            else:
+                count[0] += 1
+        elif query == 'R':
+            if reverse == True:
+                reverse = False
+            else:
+                reverse = True
+
+        if idx == len(queries)-1:
+            try:
+                for _ in range(count[0]):
+                    nums.popleft()
+                for _ in range(count[1]):
+                    nums.pop()
+            except IndexError:
+                print('error')
+                break
+            if reverse == True:
+                nums = reversed(nums)
+            print(f"[{','.join(list(map(str, nums)))}]")
